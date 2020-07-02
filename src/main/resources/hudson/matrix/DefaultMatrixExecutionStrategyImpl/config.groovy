57c4880bc7cade6dbe5dcebbc3e494166a32efa8
Mamh-Linux <bright@mage.com>
2020-07-02 16:22:14 +0800

    src: resources: add check if need to  enable the sorter for DefaultMatrixExecutionStrategyImpl
    把是否需要排序提取出来，勾选了之后然后可以选择某个排序规则进行排序。

--- src/main/resources/hudson/matrix/DefaultMatrixExecutionStrategyImpl/config.groovy
+++ src/main/resources/hudson/matrix/DefaultMatrixExecutionStrategyImpl/config.groovy
@@ -6,15 +6,16 @@ import hudson.model.Result;
 def f = namespace(lib.FormTagLib)
 
 f.optionalBlock (field:"runSequentially", title:_("Run each configuration sequentially"), inline:true) {
-    if (MatrixConfigurationSorterDescriptor.all().size()>1) {
-        f.dropdownDescriptorSelector(title:_("Execution order of builds"), field:"sorter")
+
+}
+
+f.optionalBlock (field:"hasSortExecutionbuild", title:_("Execution order of builds"), inline:true) {
+    if (MatrixConfigurationSorterDescriptor.all().size()>=1) {
+        f.dropdownDescriptorSelector(title:_("Sort rule"), field:"sorter")
     }
 }
 
 f.optionalBlock (field:"hasTouchStoneCombinationFilter", title:_("Execute touchstone builds first"), inline:true) {
-    // TODO: help="/help/matrix/touchstone.html">
-    // TODO: move l10n from MatrixProject/configEntries.jelly
-
     f.entry(title:_("Filter"), field:"touchStoneCombinationFilter") {
         f.textbox()
     }
